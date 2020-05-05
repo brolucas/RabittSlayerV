10074c728c6072f6028504db0a82a5946ec51386
Lucas Brossier <lucasbrossier95@gmail.com>
2020-05-05 12:07:53 +0200

    first commit

---
+++ RabittSlayerV/src/Joueur.java
@@ -0,0 +1,19 @@
+
+public class Joueur extends Perso{
+	
+	private int PA;
+
+	
+	public Joueur() {
+		PA=5;
+		
+	}
+	
+	public int getPA() {
+		return this.PA;
+	}
+	public void setPA( int nbr) {
+		this.PA= nbr;
+	}
+	
+}
---
+++ RabittSlayerV/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Joueur Teddy = new Joueur();
+		System.out.println(Teddy.getPA());
+		System.out.println(Teddy.getadresse());
+	}
+
+}
